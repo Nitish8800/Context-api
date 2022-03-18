602045f9915623bbab7521da923b73ee56feea70
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-19 04:40:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import { Navbar } from "./comopnents/Navbar";
+import { Body } from "./comopnents/Body";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar />
+      <Body />
     </div>
   );
 }
---
+++ src/comopnents/Card.jsx
@@ -0,0 +1,7 @@
+export const Card = () => {
+  return (
+    <div>
+      <button>Buy Now !</button>
+    </div>
+  );
+};
